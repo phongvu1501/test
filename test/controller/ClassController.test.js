ef3d437d81dfc48b977211b5120773fd36524b26
phongvudeptrai <vup3231@gmail.com>
2025-09-05 14:26:56 +0700

    unit-test

--- test/controller/ClassController.test.js
+++ test/controller/ClassController.test.js
@@ -5,20 +5,25 @@ const classController = require('../../api/controllers/ClassController');
 
 const ENVSANBOX = sinon.createSandbox();
 
+
 const Class = {
-    findOne: ENVSANBOX.stub(),
-    find: ENVSANBOX.stub(),
-    create: ENVSANBOX.stub(),
-    updateOne: ENVSANBOX.stub(),
-    destroyOne: ENVSANBOX.stub(),
-    getDatastore: ENVSANBOX.stub(),
+    findOne: () => { },
+    find: () => { },
+    create: () => { },
+    updateOne: () => { },
+    destroyOne: () => { },
+    getDatastore: () => { },
 };
-const Grade = { findOne: ENVSANBOX.stub() };
-const Student = { find: ENVSANBOX.stub() };
+const Grade = {
+    findOne: () => { }
+};
+const Student = {
+    find: () => { }
+};
 
 global.sails = {
     models: { grade: Grade, student: Student },
-    log: { error: ENVSANBOX.stub() },
+    log: { error: () => { } },
 };
 global.Class = Class;
 
@@ -39,12 +44,14 @@ describe('ClassCtr.ClassController', () => {
                 badRequest: ENVSANBOX.spy(),
                 serverError: ENVSANBOX.spy(),
             };
+            ENVSANBOX.stub(sails.log, 'error');
         });
 
         it('ClassCtr_01_01. should return success with correct data', async () => {
             const expectedResult = { id: 'c1', ...dataInput };
-            Grade.findOne.resolves({ id: 'g1' });
-            Class.create.resolves(expectedResult);
+            ENVSANBOX.stub(Grade, 'findOne').resolves({ id: 'g1' });
+            ENVSANBOX.stub(Class, 'create').resolves(expectedResult);
+
             await classController.createClass(req, res);
             expect(res.success.calledOnce).to.be.true;
         });
@@ -55,15 +62,16 @@ describe('ClassCtr.ClassController', () => {
         });
 
         it('ClassCtr_01_03. should return badRequest if grade does not exist', async () => {
-            Grade.findOne.resolves(null); 
+            ENVSANBOX.stub(Grade, 'findOne').resolves(null);
+
             await classController.createClass(req, res);
             expect(res.badRequest.calledOnce).to.be.true;
-            expect(res.badRequest.firstCall.args[0].message).to.equal('Khối lớp không tồn tại');
         });
 
         it('ClassCtr_01_04. should return serverError on create failure', async () => {
-            Grade.findOne.resolves({ id: 'g1' });
-            Class.create.rejects(new Error('DB Error')); 
+            ENVSANBOX.stub(Grade, 'findOne').resolves({ id: 'g1' });
+            ENVSANBOX.stub(Class, 'create').rejects(new Error('DB Error'));
+
             await classController.createClass(req, res);
             expect(res.serverError.calledOnce).to.be.true;
         });
@@ -82,44 +90,47 @@ describe('ClassCtr.ClassController', () => {
                 serverError: ENVSANBOX.spy(),
                 badRequest: ENVSANBOX.spy(),
             };
+            ENVSANBOX.stub(sails.log, 'error');
         });
 
         it('ClassCtr_02_01. should return success with updated data', async () => {
             const expectedResult = { id: 'c1', ...dataInput };
-            Grade.findOne.resolves({ id: 'g1' });
             const setStub = ENVSANBOX.stub().resolves(expectedResult);
-            Class.updateOne.returns({ set: setStub });
+            ENVSANBOX.stub(Grade, 'findOne').resolves({ id: 'g1' });
+            ENVSANBOX.stub(Class, 'updateOne').returns({ set: setStub });
+
             await classController.updateClass(req, res);
             expect(res.success.calledOnce).to.be.true;
         });
 
         it('ClassCtr_02_02. should return badRequest if grade does not exist', async () => {
-            Grade.findOne.resolves(null);
+            ENVSANBOX.stub(Grade, 'findOne').resolves(null);
             await classController.updateClass(req, res);
             expect(res.badRequest.calledOnce).to.be.true;
         });
 
         it('ClassCtr_02_03. should return notFound if class to update does not exist', async () => {
-            Grade.findOne.resolves({ id: 'g1' });
-            const setStub = ENVSANBOX.stub().resolves(null); 
-            Class.updateOne.returns({ set: setStub });
+            const setStub = ENVSANBOX.stub().resolves(null);
+            ENVSANBOX.stub(Grade, 'findOne').resolves({ id: 'g1' });
+            ENVSANBOX.stub(Class, 'updateOne').returns({ set: setStub });
+
             await classController.updateClass(req, res);
             expect(res.notFound.calledOnce).to.be.true;
         });
 
         it('ClassCtr_02_04. should return serverError on update failure', async () => {
-            Grade.findOne.resolves({ id: 'g1' });
             const setStub = ENVSANBOX.stub().rejects(new Error('DB Error'));
-            Class.updateOne.returns({ set: setStub });
+            ENVSANBOX.stub(Grade, 'findOne').resolves({ id: 'g1' });
+            ENVSANBOX.stub(Class, 'updateOne').returns({ set: setStub });
+
             await classController.updateClass(req, res);
             expect(res.serverError.calledOnce).to.be.true;
         });
 
         it('ClassCtr_02_05. should return badRequest if data is missing', async () => {
-            const incompleteReq = { param: () => 'c1', body: { name: 'Thiếu dữ liệu' } }; 
+            const incompleteReq = { param: () => 'c1', body: { name: 'Thiếu dữ liệu' } };
             await classController.updateClass(incompleteReq, res);
             expect(res.badRequest.calledOnce).to.be.true;
-            expect(res.badRequest.firstCall.args[0].message).to.equal('Thiếu dữ liệu');
         });
     });
 
@@ -131,33 +142,37 @@ describe('ClassCtr.ClassController', () => {
                 success: ENVSANBOX.spy(),
                 notFound: ENVSANBOX.spy(),
                 serverError: ENVSANBOX.spy(),
-                badRequest: ENVSANBOX.spy(), 
+                badRequest: ENVSANBOX.spy(),
             };
+            ENVSANBOX.stub(sails.log, 'error');
         });
 
         it('ClassCtr_03_01. should return success with deleted data', async () => {
             const req = { param: () => 'c1' };
-            Class.destroyOne.resolves({ id: 'c1' });
+            ENVSANBOX.stub(Class, 'destroyOne').resolves({ id: 'c1' });
+
             await classController.deleteClass(req, res);
             expect(res.success.calledOnce).to.be.true;
         });
 
         it('ClassCtr_03_02. should return notFound when class does not exist', async () => {
             const req = { param: () => 'c99' };
-            Class.destroyOne.resolves(null);
+            ENVSANBOX.stub(Class, 'destroyOne').resolves(null);
+
             await classController.deleteClass(req, res);
             expect(res.notFound.calledOnce).to.be.true;
         });
 
         it('ClassCtr_03_03. should return serverError on delete failure', async () => {
             const req = { param: () => 'c1' };
-            Class.destroyOne.rejects(new Error('DB Error'));
+            ENVSANBOX.stub(Class, 'destroyOne').rejects(new Error('DB Error'));
+
             await classController.deleteClass(req, res);
             expect(res.serverError.calledOnce).to.be.true;
         });
 
         it('ClassCtr_03_04. should return badRequest if id is missing', async () => {
-            const req = { param: () => null }; 
+            const req = { param: () => null };
             await classController.deleteClass(req, res);
             expect(res.badRequest.calledOnce).to.be.true;
         });
@@ -172,34 +187,39 @@ describe('ClassCtr.ClassController', () => {
                 notFound: ENVSANBOX.spy(),
                 serverError: ENVSANBOX.spy(),
             };
+            ENVSANBOX.stub(sails.log, 'error');
         });
 
         it('ClassCtr_04_01. should return a list of all classes', async () => {
             const req = { param: () => null };
             const sortStub = ENVSANBOX.stub().resolves([]);
-            Class.find.returns({ sort: sortStub });
+            ENVSANBOX.stub(Class, 'find').returns({ sort: sortStub });
+
             await classController.showClass(req, res);
             expect(res.success.calledOnce).to.be.true;
         });
 
         it('ClassCtr_04_02. should return a single class with students', async () => {
             const req = { param: () => 'c1' };
-            Class.findOne.resolves({ id: 'c1' });
-            Student.find.resolves([]);
+            ENVSANBOX.stub(Class, 'findOne').resolves({ id: 'c1' });
+            ENVSANBOX.stub(Student, 'find').resolves([]);
+
             await classController.showClass(req, res);
             expect(res.success.calledOnce).to.be.true;
         });
 
         it('ClassCtr_04_03. should return notFound if class does not exist', async () => {
             const req = { param: () => 'c99' };
-            Class.findOne.resolves(null); 
+            ENVSANBOX.stub(Class, 'findOne').resolves(null);
+
             await classController.showClass(req, res);
             expect(res.notFound.calledOnce).to.be.true;
         });
 
         it('ClassCtr_04_04. should return serverError on find failure', async () => {
             const req = { param: () => null };
-            Class.find.rejects(new Error('DB Error'));
+            ENVSANBOX.stub(Class, 'find').rejects(new Error('DB Error'));
+
             await classController.showClass(req, res);
             expect(res.serverError.calledOnce).to.be.true;
         });
@@ -213,6 +233,7 @@ describe('ClassCtr.ClassController', () => {
                 success: ENVSANBOX.spy(),
                 serverError: ENVSANBOX.spy(),
             };
+            ENVSANBOX.stub(sails.log, 'error');
         });
 
         it('ClassCtr_05_01. should return search results', async () => {
@@ -222,7 +243,8 @@ describe('ClassCtr.ClassController', () => {
             const findStub = ENVSANBOX.stub().returns({ limit: limitStub });
             const collectionStub = { find: findStub };
             const managerStub = { collection: ENVSANBOX.stub().returns(collectionStub) };
-            Class.getDatastore.returns({ manager: managerStub });
+            ENVSANBOX.stub(Class, 'getDatastore').returns({ manager: managerStub });
+
             await classController.searchClass(req, res);
             expect(res.success.calledOnce).to.be.true;
         });
@@ -235,7 +257,8 @@ describe('ClassCtr.ClassController', () => {
 
         it('ClassCtr_05_03. should return serverError on search failure', async () => {
             const req = { params: { query: 'error' } };
-            Class.getDatastore.rejects(new Error('DB Error'));
+            ENVSANBOX.stub(Class, 'getDatastore').rejects(new Error('DB Error'));
+
             await classController.searchClass(req, res);
             expect(res.serverError.calledOnce).to.be.true;
         });
